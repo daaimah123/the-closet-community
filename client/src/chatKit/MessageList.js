fbc4f27076d0734f33c500aac72168cd59c346f3
chisomagajika <43393909+chisomagajika@users.noreply.github.com>
2019-05-16 10:44:03 -0700

    add the MessageList page and created a chat room

---
+++ client/src/chatKit/MessageList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MessageList extends React.Component{
+    render () {
+        return <div>
+            {this.props.messages.map((message, index) =>(
+                dfdfd
+            ))}
+        </div>
+    }
+}
+
+export default MessageList;
